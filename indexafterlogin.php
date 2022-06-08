002c84747572ee33aafa1ba170485bba02c2f4bf
Thanushree S <100961189+Thanu2114@users.noreply.github.com>
2022-06-08 20:50:22 +0530

    Add files via upload

---
+++ indexafterlogin.php
@@ -0,0 +1,105 @@
+<?php
+include("connection.php");
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LeafNow</title>
+    <link rel="stylesheet" href="/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Kanit:wght@600;800&display=swap" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    
+    <script src="/script.js"></script>
+    <meta name="viewport" content="width=device-width, intial-scale=1.0">
+    <meta http-equive="X-UA-Compatible" content="ie=edge">
+
+</head>
+
+<body>
+
+
+
+    <nav class="navibar bg-lightt">
+        <div class="container-fluidd">
+            <a class="navbar-brand" href="#"><strong>LeafNow</strong></a>
+
+        </div>
+        
+        <button class="btn1 btn-lightt">GO BACK</button>
+    </nav>
+    <!-- --------------Home Section---------------------------- -->
+
+    <section id="home">
+        
+     <h1 class="h-primary">WHY WE DO WHAT WE DO?</h1>
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque quaerat ipsa modi quas omnis reiciendis in laudantium esse rem atque, provident et numquam ab aperiam placeat adipisci, sequi quod at?</p>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod deleniti numquam nostrum. Natus quam autem, saepe similique delectus repellat placeat! Reiciendis vero, ipsum dicta itaque magni delectus doloremque officiis asperiores?</p>
+     
+    </section>
+    <!-- -------------------------------About Section----------------------------------------------------------------- -->
+    <section class="service-container">
+           <h1 class="h-primary center">About </h1>
+           <div id="services">
+               <div class="box">
+                <img src="/plantWithSunlight.jpg" alt="plant">
+                <h2 class="h-secondary center">Buy</h2>
+                <p class="center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur et porro optio dignissimos aut odio id corporis nesciunt veniam tenetur consectetur assumenda, earum impedit ratione accusamus a corrupti quis minima quaerat atque. Labore, sit!</p>
+                <div class="button1">
+
+                    <button class="button1"><a href="/compsoft/clientpurchase.php">Buy</a></button>
+                </div>
+               </div>
+               <div class="box">
+                <img src="/flower3.jpg" alt="plant">
+                <h2 class="h-secondary center">Sell</h2>
+                <p class="center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur et porro optio dignissimos aut odio id corporis nesciunt veniam tenetur consectetur assumenda, earum impedit ratione accusamus a corrupti quis minima quaerat atque. Labore, sit!</p>
+                <div class="button1">
+
+                    <button class="button1"><a href="/compsoft/sell.php">SELL</a></button>
+                </div>
+               
+               </div>
+               <div class="box">
+                <img src="/flower3.jpg" alt="plant">
+                <h2 class="h-secondary center"><a href="/compsoft/sell.php">DONATE</a></h2>
+                <p class="center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur et porro optio dignissimos aut odio id corporis nesciunt veniam tenetur consectetur assumenda, earum impedit ratione accusamus a corrupti quis minima quaerat atque. Labore, sit!</p>
+                <div class="button1">
+
+                    <button class="button1">DONATE</button>
+                </div>
+                
+               </div>
+               
+           </div>
+    </section>
+
+
+
+
+
+    </nav>
+
+</body>
+
+</html>
